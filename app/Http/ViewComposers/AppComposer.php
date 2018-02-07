b38589b8f44351a5934d590e65626adf6c45607d
TJ Barber <me@tjbarber.org>
2018-02-07 12:54:38 -0700

    We display if we're logged in everywhere now

---
+++ app/Http/ViewComposers/AppComposer.php
@@ -0,0 +1,31 @@
+<?php namespace App\Http\ViewComposers;
+ 
+use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Auth;
+
+class AppComposer {
+
+    protected $isLoggedIn;
+    protected $user;
+
+
+    public function __construct()
+    {
+        // Dependencies automatically resolved by service container...
+        $this->isLoggedIn = Auth::check();
+        $this->user = Auth::user();
+    }
+
+    /**
+     * Bind data to the view.
+     *
+     * @param  View  $view
+     * @return void
+     */
+    public function compose(View $view)
+    {
+        $view->with('isLoggedIn', $this->isLoggedIn);
+        $view->with('user', $this->user);
+    }
+
+}
