bee10b544ba489166eb80fe8a50f99f16c026352
TJ Barber <me@tjbarber.org>
2018-07-11 14:43:32 -0600

    Removed old JS from webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,5 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .js('resources/assets/js/massrunuploader.js', 'public/js')
-   .js('resources/assets/js/masshumanuploader.js', 'public/js')
-   .js('resources/assets/js/runeditor.js', 'public/js')
-   .js('resources/assets/js/userhumanlinker.js', 'public/js')
-   .js('resources/assets/js/profile.js', 'public/js')
-   .js('resources/assets/js/videoplayer.js', 'public/js')
-   .js('resources/assets/js/createhuman.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
+   .sass('resources/assets/sass/app.scss', 'public/css');
 
