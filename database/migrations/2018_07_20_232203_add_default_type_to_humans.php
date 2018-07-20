27f8b8e889ba8f2cb564752ac6715af0de191f73
TJ Barber <me@tjbarber.org>
2018-07-20 17:24:18 -0600

    added default to type column on humans

---
+++ database/migrations/2018_07_20_232203_add_default_type_to_humans.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDefaultTypeToHumans extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('humans', function (Blueprint $table) {
+            $table->string('type')->default('standard')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('humans', function (Blueprint $table) {
+            $table->string('type')->default(null)->change();
+        });
+    }
+}
