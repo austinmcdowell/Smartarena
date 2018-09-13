4a377d36b44625882b6f290d405a4571302e45fd
TJ Barber <me@tjbarber.org>
2018-09-13 13:09:02 -0600

    Added small jquery code to collapse navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -121,4 +121,9 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     @yield('javascript')
     <script src="/js/app.js"></script>
+    <script>
+    $('.navbar-nav>li>a').on('click', function(){
+        $('.navbar-collapse').collapse('hide');
+    });
+    </script>
 </html>
